0333937ae5317b5f91f0440cc94e0ef09f476f26
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-10-08 21:02:29 +0200

    export script (needs a rules-file)

---
+++ 03.migrate/export.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+RULES=$1
+SVNPATH=$(realpath ${0%/*}/../01.backup/svn)
+AUTHORS=$(realpath ${0%/*}/AUTHORS.txt)
+
+error() {
+  echo "$@" 1>&2
+}
+
+if [ -e "${RULES}" ]; then
+ :
+else
+ error "unable to read rules file: ${RULES}"
+ exit 1
+fi
+
+if [ ! -d "${SVNPATH}" ]; then
+ error "unable to find SVN-repository ${SVNPATH}"
+ exit 1
+fi
+
+if [ ! -e "${AUTHORS}" ]; then
+ error "unable to find authors-map ${AUTHORS}"
+ exit 1
+fi
+
+svn-all-fast-export --identity-map="${AUTHORS}" --rules="${RULES}" "${SVNPATH}"
