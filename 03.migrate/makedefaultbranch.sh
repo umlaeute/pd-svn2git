d111b82d51d72688f6edc4638699c61abb1429ba
IOhannes m zmölnig <zmoelnig@umlautQ.umlaeute.mur.at>
2015-10-13 14:25:59 +0200

    verbose default-branch creation

--- 03.migrate/makedefaultbranch.sh
+++ 03.migrate/makedefaultbranch.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
+
 if [ "${1}" ]; then
  cd "${1}"
 fi
 
-for j in $(find refs/heads/ -type f)
-do
- git symbolic-ref HEAD $j
-done
+if [ -e refs/heads/master ]; then
+ echo "make 'master' default branch for $1"
+ git symbolic-ref HEAD refs/heads/master
+else
+ for j in $(find refs/heads/ -type f)
+ do
+ echo "make '${j}' default branch for $1"
+  git symbolic-ref HEAD $j
+ done
+fi
